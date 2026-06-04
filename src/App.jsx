07bc03c1c19734cf6af8b4bb6fc70e559c679f90
Abindavis508 <abindavis18@gmail.com>
2026-06-04 11:02:58 +0530

    new footer  componet added to the home page and some typo changes in the carcard.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import CarDetails from './pages/CarDetails'
 import Cars from './pages/Cars'
 import MyBookings from './pages/MyBookings'
+import Footer from './components/Footer'
 
 const App = () => {
 
-  const [showLogin, setShowLogin]= useState(false)
-  const isOwnerpath =useLocation().pathname.startsWith('/owner')
+  const [showLogin, setShowLogin] = useState(false)
+  const isOwnerpath = useLocation().pathname.startsWith('/owner')
 
   return (
     <>
       {!isOwnerpath && <Navbar setShowLogin={setShowLogin} />}
 
       <Routes>
-        <Route path ='/' element={<Home/>}/>
-        <Route path ='/car-details/:id' element={<CarDetails/>}/>
-        <Route path ='/cars' element={<Cars/>}/>
-        <Route path ='/my-bookings' element={<MyBookings/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/car-details/:id' element={<CarDetails />} />
+        <Route path='/cars' element={<Cars />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
 
       </Routes>
-      
+
+      { !isOwnerpath && <Footer /> }
+   
     </>
   )
 }
--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -12,11 +12,11 @@ const CarCard = ({ car }) => {
     className='group rounded-xl overflow-hidden shadow-lg hover:-translate-y-1
     transition-all duration-500 cursor-pointer'>
 
-      <div className='realtive h-48 overflow-hidden'>
+      <div className='relative h-48 overflow-hidden'>
         <img src={car.image} alt="car image" className='w-full h-full object-cover
          transition-transform duration-500 group-hover:scale-105'/>
 
-        {car.isAvailable && <p className='absolute top-4 left-4 bg-primary/90text-white 
+        {car.isAvailable && <p className='absolute top-4 left-4 bg-primary/90 text-white 
         text-xs px-2.5 py-1 rounded-full'>Available Now</p>}
 
         <div className="absolute bottom-4 right-4 bg-black/80 backdrop-blur-sm
@@ -28,12 +28,12 @@ const CarCard = ({ car }) => {
 
       </div>
 
-      <div classname='p-4 sm:p-5'>
+      <div className='p-4 sm:p-5'>
 
         <div className="flex justify-between items-start mb-2">
 
           <div className="">
-            <h3 calssName='text-lg font-medium'>{car.brand} {car.model}</h3>
+            <h3 className='text-lg font-medium'>{car.brand} {car.model}</h3>
             <p className='text-muted-foreground text-sm'>{car.category} {car.year}</p>
           </div>
 
